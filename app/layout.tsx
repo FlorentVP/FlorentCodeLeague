55e98fd8cb42c955f980d2826793635d4e40c035
Nolan Gray <nolan@florent.vc>
2026-06-02 12:55:33 +0100

    Set bot-king as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import LoadingScreen from "./components/LoadingScreen";
 export const metadata: Metadata = {
   title: "Florent Code League 2026",
   description: "Find out if you're in the top 1% of programmers in the Nordics.",
+  icons: {
+    icon: '/bot-king.png',
+  },
 };
 
 export default function RootLayout({
